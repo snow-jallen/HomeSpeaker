df861e050842459339b1e61832629d5e4a60c306
Audrey Inglish <audreyinglish@gmail.com>
2025-02-24 20:46:54 -0700

    basic MAUI project setup

---
+++ HomeSpeaker.Maui/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace HomeSpeaker.Maui;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ HomeSpeaker.Maui/Views/ManageDevicesView.xaml.cs
@@ -0,0 +1,9 @@
+namespace HomeSpeaker.Maui.Views;
+
+public partial class ManageDevicesView : ContentPage
+{
+	public ManageDevicesView()
+	{
+		InitializeComponent();
+	}
+}
