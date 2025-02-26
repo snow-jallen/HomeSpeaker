b9b2eae59e73b97bcb7ba1c2e73062b8c397ecba
zane Clegg <zane.clegg@students.snow.edu>
2025-02-26 12:06:40 -0700

    Added an interface to structure the implementation of features

---
+++ HomeSpeaker.MAUI/Models/SongGroup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeSpeaker.MAUI.Models
+{
+    public partial class SongGroup : List<SongModel>
+    {
+        string FolderName { get; set; }
+        string FolderPath { get; set; }
+
+        public SongGroup(string name, List<SongModel> songs) : base(songs)
+        {
+            FolderName = name;
+            FolderPath = name;
+        }
+    }
+}
