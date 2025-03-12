d6b6be608838c115eab7c4d69725c20434fd7b24
Audrey Inglish <audreyinglish@gmail.com>
2025-03-12 01:14:45 -0600

    styling is ok

--- HomeSpeaker.Maui/MauiProgram.cs
+++ HomeSpeaker.Maui/MauiProgram.cs
@@ -17,6 +17,7 @@ public static class MauiProgram
             {
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                 fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                fonts.AddFont("fa-solid.otf", "FontAwesomeSolid");
             })
             .RegisterViewModels()
             .RegisterViews();
