29080cd0c9caad73b01d3ddfd2dba7ea60cf695d
Audrey Inglish <audreyinglish@gmail.com>
2025-03-14 13:53:22 -0600

    refactored for a wayyy better implementation of slider

--- HomeSpeaker.Maui/Views/MusicController.xaml.cs
+++ HomeSpeaker.Maui/Views/MusicController.xaml.cs
@@ -22,30 +22,4 @@ public partial class MusicController : ContentPage
         //await _vm.Initialize();
     }
 
-    private CancellationTokenSource _cts;
-
-    private void OnSliderValueChanged(object sender, ValueChangedEventArgs e)
-    {
-        // dispose cts and create a new one
-        if (_cts != null)
-        {
-            _cts.Cancel();
-            _cts.Dispose();
-        }
-
-        _cts = new CancellationTokenSource();
-
-        Task.Delay(300, _cts.Token)
-            .ContinueWith(async t =>
-            {
-                // if the token in the cts doesn't have a cancellation request, call SetVolumeAsync
-                if (!t.IsCanceled) 
-                {
-                    _vm.VolumeInput = (int)e.NewValue;
-                    await _vm.SetVolumeAsync();
-                }
-            }, TaskScheduler.FromCurrentSynchronizationContext()); // tells the task to continue on the same thread after the delay
-    }
-
-
 }
